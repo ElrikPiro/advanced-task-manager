1b7ee7e31e705646fea03bcf9e1e325216f0d48e
David Baselga <baselga.david@gmail.com>
2024-08-23 08:15:02 +0200

    roadmap updated

--- backend/backend-obsidian.py
+++ backend/backend-obsidian.py
@@ -86,3 +86,4 @@ if __name__ == '__main__':
 ## 4. Refactor TelegramReportingService to separate concerns
 ## 5. Crear un sistema de maquina de estados para el servicio de telegram, ejemplos: "Escuchando ordenes, creando tarea, (...)"
 ## 6. Implementar un "/import" para importar tareas desde un archivo json y un "/export" para exportar tareas a un archivo json
+## 7. Pon getId() a ITaskModel de tal modo que una tarea pueda ser seleccionada por su id 
