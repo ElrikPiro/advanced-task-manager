967faf8d5b48e551d05aea43753bef031f90e437
David Baselga <emperador.albino@gmail.com>
2025-02-25 17:07:22 +0100

    Add unit tests for FileBroker methods to enhance coverage and functionality

--- backend/tests/FileBroker_test.py
+++ backend/tests/FileBroker_test.py
@@ -17,7 +17,7 @@ class TestFileBroker(unittest.TestCase):
             self.vaultPath
         )
 
-    def test_readFileContent(self):  # unwatched test
+    def test_readFileContent_WhenFileIsFound_ThenReturnFileContent(self):
         with patch("builtins.open", mock_open(read_data="data")) as mock_file:
             readcontent = self.fileBroker.readFileContent(
                 FileRegistry.STANDALONE_TASKS_JSON
@@ -25,6 +25,37 @@ class TestFileBroker(unittest.TestCase):
             self.assertEqual(readcontent, "data")
             filePath = os.path.join(self.jsonPath, "tasks.json")
             mock_file.assert_called_once_with(filePath, "r")
+
+    def test_readFileContent_WhenFileIsNotFound_ThenCreateFileAndReturnDefaultContent(self):
+        with patch.object(self.fileBroker, '_FileBroker__createFile', return_value=None):
+            with patch("builtins.open", side_effect=FileNotFoundError):
+                readcontent = self.fileBroker.readFileContent(
+                    FileRegistry.STANDALONE_TASKS_JSON
+                )
+                self.assertEqual(readcontent, '{"tasks": [], "pomodoros_per_day": "2"}')
+
+    def test_readFileContentJson_WhenFileIsFound_ThenReturnFileContent(self):
+        with patch("builtins.open", mock_open(read_data='{"key": "value"}')) as mock_file:
+            readcontent = self.fileBroker.readFileContentJson(
+                FileRegistry.STANDALONE_TASKS_JSON
+            )
+            self.assertEqual(readcontent, {"key": "value"})
+            filePath = os.path.join(self.jsonPath, "tasks.json")
+            mock_file.assert_called_once_with(filePath, "r")
+
+    def test_readFileContentJson_WhenFileIsNotFound_ThenCreateFileAndReturnDefaultContent(self):
+        with patch.object(self.fileBroker, '_FileBroker__createFile', return_value=None):
+            with patch("builtins.open", side_effect=FileNotFoundError):
+                readcontent = self.fileBroker.readFileContentJson(
+                    FileRegistry.STANDALONE_TASKS_JSON
+                )
+                self.assertEqual(readcontent, {"tasks": [], "pomodoros_per_day": "2"})
+
+    def test___createFile_WhenFileIsCreated_ThenReturnNone(self):
+        with patch("builtins.open", mock_open()) as mock_file:
+            self.fileBroker._FileBroker__createFile(FileRegistry.STANDALONE_TASKS_JSON)
+            filePath = os.path.join(self.jsonPath, "tasks.json")
+            mock_file.assert_called_once_with(filePath, "w+")
 
 
 if __name__ == "__main__":
