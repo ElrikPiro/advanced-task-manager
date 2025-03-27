fa9af9683a1c1982b60b292724c14dbd50e68bec
David Baselga <emperador.albino@gmail.com>
2025-03-27 16:00:35 +0100

    Enhance IStatisticsService documentation for doWork and getWorkDoneLog methods

--- backend/src/Interfaces/IStatisticsService.py
+++ backend/src/Interfaces/IStatisticsService.py
@@ -9,6 +9,14 @@ class IStatisticsService(ABC):
 
     @abstractmethod
     def doWork(self, date: datetime.date, work_units: TimeAmount, task: ITaskModel):
+        """
+        Records work done on a task on a given date.
+
+        Params:
+            date: The date on which the work was done.
+            work_units: The amount of work done.
+            task: The task on which the work was done.
+        """
         pass
 
     @abstractmethod
@@ -25,4 +33,14 @@ class IStatisticsService(ABC):
 
     @abstractmethod
     def getWorkDoneLog(self) -> list:
+        """
+        Returns a list of dictionaries representing the work done log.
+
+        Returns:
+            A list of dictionaries representing the work done log.
+            Fields are the following:
+                - timestamp: The timestamp of the work done.
+                - work_units: The amount of work done.
+                - task: The task on which the work was done.
+        """
         pass
