90b62f3941fe12ea8ea6737065b7ee9f9bd8e268
David Baselga <emperador.albino@gmail.com>
2025-04-03 16:59:47 +0200

    Add docstrings to IHeuristic and ITaskListManager interfaces for clarity

--- backend/src/Interfaces/IHeuristic.py
+++ backend/src/Interfaces/IHeuristic.py
@@ -7,12 +7,24 @@ class IHeuristic(ABC):
 
     @abstractmethod
     def sort(self, tasks: List[ITaskModel]) -> List[Tuple[ITaskModel, float]]:
+        """
+        Sorts a list of tasks based on the heuristic evaluation.
+        Returns a list of tuples (task, value) sorted in a logical order.
+        """
         pass
 
     @abstractmethod
     def evaluate(self, task: ITaskModel) -> float:
+        """
+        Evaluates the heuristic for a given task.
+        Returns a float value representing the heuristic's evaluation.
+        """
         pass
 
     @abstractmethod
     def getComment(self, task: ITaskModel) -> str:
+        """
+        Returns a string with the heuristic's comment for the task.
+        This is used for displaying the heuristic value in the UI.
+        """
         pass
