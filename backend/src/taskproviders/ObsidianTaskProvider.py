37124a12c525854a8a3de93aa17c2e5c020b2456
David Baselga <emperador.albino@gmail.com>
2025-04-30 15:15:15 +0200

    Hotfix: remove duplication

--- backend/src/taskproviders/ObsidianTaskProvider.py
+++ backend/src/taskproviders/ObsidianTaskProvider.py
@@ -64,8 +64,8 @@ class ObsidianTaskProvider(ITaskProvider):
             return self.TaskJsonProvider.getJson()[string]
 
     def _getTaskLine(self, task: ITaskModel) -> str:
-        description = task.getDescription().split("@")[0].strip()
         context = task.getContext()
+        description = task.getDescription().split("@")[0].strip().split(f"({context})")[:-1].strip()
         start = str(task.getStart())
         due = str(task.getDue())
         severity = task.getSeverity()
